193b4810838567f2d5af5fddfd6884b11bad30a3
Marcosky1 <yaperdilacabeza@outlook.com>
2024-09-17 11:46:12 -0500

    ThePlayerCanJumpButWithoutLimit

--- My project (3)/Assets/Scripts/SimplePlayerController.cs
+++ My project (3)/Assets/Scripts/SimplePlayerController.cs
@@ -25,29 +25,33 @@ public class SimplePlayerController : MonoBehaviour
     public void movementDirection(InputAction.CallbackContext context)
     {
         direction = context.ReadValue<Vector2>();
-        direction = new Vector3(direction.x, 0, direction.y);
     }
-    //bool onTheFloor()
-   // {
-      //  RaycastHit raycastHit = Physics.BoxCast(boxCollider.bounds.center, new Vector3(boxCollider.bounds.size.x, boxCollider.bounds.size.y), 0f, Vector3.down, 0.2f, floorLayer);
-        //return raycastHit.collider != null;
-    //}
-   // public void Jumps(InputAction.CallbackContext context)
-   // {
-   //     float inputJump = context.ReadValue<float>();
-   //     if (onTheFloor())
-   //     {
-   //         remainingJumps = maxJumps;
-   //     }
-   //     if (context.performed && remainingJumps > 0)
-   //     {
-   //         remainingJumps = remainingJumps - 1;
-   //         movementP.velocity = new Vector3(movementP.velocity.x, 0f);
-   //     }
-   // }
+    //
+  //  bool onTheFloor()
+  //  {
+  //      RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, new Vector2(boxCollider.bounds.size.x, boxCollider.bounds.size.y), 0f, Vector2.down, 0.2f, floorLayer);
+  //      return raycastHit.collider != null;
+  //  }
+    public void Jumps(InputAction.CallbackContext context)
+   {
+      //     float inputJump = context.ReadValue<float>();
+      //     if (onTheFloor())
+      //     {
+      //         remainingJumps = maxJumps;
+      //     }
+      //     if (context.performed && remainingJumps > 0)
+      //     {
+      //         remainingJumps = remainingJumps - 1;
+            //
+            movementP.velocity = new Vector3(movementP.velocity.x, 0f);
+            movementP.AddForce(Vector2.up * jumpForce, ForceMode.Impulse);
+            //
+      //     }
+    }
+    //
     private void FixedUpdate()
     {
-        Vector3 movement = direction * velocity;
+        Vector3 movement = new Vector3(direction.x * velocity, movementP.velocity.y, direction.y * velocity);
         movementP.velocity = movement;
     }
 }
